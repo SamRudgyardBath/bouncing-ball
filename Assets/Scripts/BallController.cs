099d23ed6efda917936b207fc7957b93077e351d
SamRudgyardBath <sr2116@bath.ac.uk>
2023-06-17 18:57:18 +0100

    Set Initial Velocity and Acceleration for Ball

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -17,4 +17,21 @@ public class BallController : MonoBehaviour
     private bool isWithinBox;
     private int noOfSubsteps = 5;
     #endregion
+
+    // Start is called before the first frame update
+    void Start() {
+        // Initialise position and velocity
+        pos = worldLoc.position;
+        v = new Vector3(0, 0, 1);
+        a = new Vector3(0, -9.81f, 0);
+        radius = Random.value * 5f;
+        transform.localScale = new Vector3(radius, radius, radius);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Update the object's position
+        worldLoc.position = pos;
+    }
+
 }
